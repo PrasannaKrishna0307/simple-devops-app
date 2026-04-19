3a48ecc4fc70fbcf746fb78a21e8576091d181f8
PrasannaKrishna0307 <prasannakrishna257@gmail.com>
2026-04-19 10:56:21 +0530

    backend update and updated deploy.yml

--- simple-devops-app/backend/server.js
+++ simple-devops-app/backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res) => {
-    res.send("Hello from Backend");
+    res.send("Updated Backend");
 });
 
 app.listen(3000, () => {
