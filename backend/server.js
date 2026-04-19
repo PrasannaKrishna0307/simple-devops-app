b7678d34843584a5aa95e8e4c276b91e21e6eef6
PrasannaKrishna0307 <prasannakrishna257@gmail.com>
2026-04-19 11:11:12 +0530

    Updated deploy.yml

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res) => {
-    res.send("Updated Backend");
+    res.send("Updated Backend - Prasanna");
 });
 
 app.listen(3000, () => {
